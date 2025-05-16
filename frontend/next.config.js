c3661b13c8abb0cf9d3ebbd630e0d3ffbe1dc1c1
se-like <mkoji530@gmail.com>
2025-05-16 08:29:10 +0000

    fix: reCAPTCHAの表示を修正するためCSPを更新

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -32,7 +32,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:; frame-src 'self' https://www.google.com/recaptcha/ https://recaptcha.google.com/recaptcha/;"
           }
         ]
       }
